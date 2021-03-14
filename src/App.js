77dba70f2c80efc5b27d5f9677e64c5c3478389b
BlueZeeKing <braydenzee+git@gmail.com>
2021-03-14 12:57:40 -0400

    finished small board

--- src/App.js
+++ src/App.js
@@ -16,32 +16,36 @@ class Board extends React.Component {
     super(props)
 
     this.state = {
-      board: props.board
+      board: props.board,
     }
+
+    console.log(this.isFull())
+
+    this.state.open = !this.isFull() && (this.hasWon() === '')
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   isFull () {
-    return this.state.board.filter(function (item) { return item === 'n' }).length === 0
+    return this.state.board.filter(function (item) { return item === '' }).length === 0
   }
 
   hasWon() {
-    if (this.state.board[0] == this.state.board[1] && this.state.board[0] == this.state.board[2]) {
+    if (this.state.board[0] === this.state.board[1] && this.state.board[0] === this.state.board[2]) {
       return this.state.board[0]
-    } else if (this.state.board[3] == this.state.board[4] && this.state.board[3] == this.state.board[5]) {
+    } else if (this.state.board[3] === this.state.board[4] && this.state.board[3] === this.state.board[5]) {
       return this.state.board[3]
-    } else if (this.state.board[6] == this.state.board[7] && this.state.board[6] == this.state.board[8]) {
+    } else if (this.state.board[6] === this.state.board[7] && this.state.board[6] === this.state.board[8]) {
       return this.state.board[6]
-    } else if (this.state.board[0] == this.state.board[3] && this.state.board[0] == this.state.board[6]) {
+    } else if (this.state.board[0] === this.state.board[3] && this.state.board[0] === this.state.board[6]) {
       return this.state.board[0]
-    } else if (this.state.board[1] == this.state.board[4] && this.state.board[1] == this.state.board[7]) {
+    } else if (this.state.board[1] === this.state.board[4] && this.state.board[1] === this.state.board[7]) {
       return this.state.board[1]
-    } else if (this.state.board[2] == this.state.board[5] && this.state.board[2] == this.state.board[8]) {
+    } else if (this.state.board[2] === this.state.board[5] && this.state.board[2] === this.state.board[8]) {
       return this.state.board[2]
-    } else if (this.state.board[0] == this.state.board[4] && this.state.board[0] == this.state.board[8]) {
+    } else if (this.state.board[0] === this.state.board[4] && this.state.board[0] === this.state.board[8]) {
       return this.state.board[0]
-    } else if (this.state.board[2] == this.state.board[4] && this.state.board[2] == this.state.board[6]) {
+    } else if (this.state.board[2] === this.state.board[4] && this.state.board[2] === this.state.board[6]) {
       return this.state.board[2]
     } else {
       return ''
@@ -49,7 +53,16 @@ class Board extends React.Component {
   }
 
   handleClick (e) {
-    if (this.props.active) {
+    if (this.props.active && this.state.open) {
+      let state = this.state
+
+      state.board[e] = this.props.currentTurn
+      state.open = !this.isFull() && (this.hasWon() === '')
+
+      this.setState(state)
+
+      this.props.doneHandler(e, this.hasWon(), state.open, state.board)
+
       console.log(e)
     }
   }
@@ -73,7 +86,7 @@ function Square (props) {
   return (
     <div className="w-1/3 h-1/3 border border-gray-400 flex flex-col float-left" onClick={function (e) { props.onClick(props.index) }}>
       <div className="flex-grow"></div>
-      <p className="text-center">{props.value}</p>
+      <p className="text-center cursor-default">{props.value}</p>
       <div className="flex-grow"></div>
     </div>
   )
