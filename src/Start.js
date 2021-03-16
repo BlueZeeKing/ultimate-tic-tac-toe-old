022f96b7a6bdbbf88452a96179e469de74f82f89
BlueZeeKing <braydenzee+git@gmail.com>
2021-03-16 11:00:37 -0400

    Added start styling

---
+++ src/Start.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header.js";
+
+function StartView(props) {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  return(
+    <div className = "w-screen h-screen" >
+      <Header />
+      <div className="flex flex-col text-center h-full w-full">
+        <div className="flex-grow"></div>
+        <div className="flex flex-row text-center w-full">
+          <div className="flex-grow"></div>
+          <div className="">
+            <Form />
+          </div>
+          <div className="flex-grow"></div>
+        </div>
+        <div className="flex-grow"></div>
+      </div>
+    </div>
+  )
+}
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      otherPlayer: ''
+    }
+
+    this.changeHandler = this.changeHandler.bind(this)
+  }
+
+  changeHandler(e) {
+    let state = this.state
+
+    state[e.target.name] = e.target.value
+
+    this.setState(state)
+  }
+
+  render() {
+    return (
+      <div className="p-12 border-2 border-blue-500 rounded-lg">
+        <input className="rounded-none border-b-2 outline-none focus:outline-none border-blue-500 focus:border-green-500 transition duration-200 m-1 my-4" value={this.state.username} onChange={this.changeHandler} id="username" name="username" type="text" placeholder="Username" />
+        <br />
+        <input className="rounded-none border-b-2 outline-none focus:outline-none border-blue-500 focus:border-green-500 transition duration-200 m-1 my-4" value={this.state.otherPlayer} onChange={this.changeHandler} id="otherPlayer" name="otherPlayer" type="text" placeholder="Other Player" />
+        <br />
+        <button className="m-1 my-4 border-blue-500 border-2 rounded text-blue-500 focus:text-white bg-white focus:bg-blue-500 px-4 p-1 transition duration-200 outline-none focus:outline-none">Play</button>
+      </div>
+    )
+  }
+}
+
+export default StartView
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import GameView from './Game';
+import StartView from './Start';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GameView />
+    <StartView />
   </React.StrictMode>,
   document.getElementById('root')
 );
